be5b3587184951be7d002d8a99e5bb49ea69d593
Ben Stav <benstav@gmail.com>
2012-01-15 18:58:47 +0200

    Changed build script to support multiple compilers, chooses smallest one

--- build.js
+++ build.js
@@ -5,30 +5,68 @@ var http = require( 'http' ),
 var originalName = 'game.js',
 	compressedName = 'game-min.js';
 
-var compile = function ( params ) {
-	console.log( 'Sending request...' );
 
-	var req = http.request({
+//indiviaul compiler API info
+var compilers = [
+	{
+		name : 'closure compiler',
 		host : 'closure-compiler.appspot.com',
 		path : '/compile',
-		headers : {
-			'content-type' : 'application/x-www-form-urlencoded'
+		params : {
+			compilation_level : 'SIMPLE_OPTIMIZATIONS',
+			output_info : 'compiled_code',
+			output_format : 'text'
 		},
-		method : 'POST'
-	}, compileEnd);
-
-	req.end( querystring.stringify(params) );
-
-	function compileEnd ( resp ) {
-		resp.on( 'data', function ( body ) {
-
-			var res = JSON.parse( body.toString() ),
-			code = lastChanges( res.compiledCode );
-
-			console.log( 'Saving compressed code...\n' );
-			fs.writeFile( compressedName, code, saveEnd );
-		});
+		codeParamName : 'js_code'
+	},
+	{
+		name : 'uglify',
+		host : 'marijnhaverbeke.nl',
+		path : '/uglifyjs',
+		codeParamName : 'js_code'
 	}
+];
+
+var compile = function ( code, callback ) {
+	var compiled = [], finished = 0;
+
+	compilers.forEach(function ( compiler ) {
+		console.log( 'sending request to ' + compiler.name );
+		var req = http.request({
+			host : compiler.host,
+			path : compiler.path,
+			method : 'POST',
+			headers : {
+				'content-type' : 'application/x-www-form-urlencoded'
+			}
+		}, compileEnd );
+
+		var params = compiler.params || {};
+		params[ compiler.codeParamName ] = code;
+
+		req.end( querystring.stringify(params) );
+
+		function compileEnd ( resp ) {
+			var compiledCode = '';
+			resp.on( 'data', function ( body ) {
+				compiledCode += body.toString();
+			});
+
+			resp.on( 'end', function () {
+				compiledCode = lastChanges( compiledCode );
+				compiled.push( compiledCode );
+
+				console.log( compiler.name + ' compilation size: ' + compiledCode.length );
+				finished++;
+
+				//finished count eq/gt than the number we want, we're done
+				if ( finished >= compilers.length ) {
+					callback( compiled );
+				}
+			});
+		}
+	});
+	console.log( '' );
 
 	function lastChanges ( code ) {
 		return code
@@ -37,39 +75,48 @@ var compile = function ( params ) {
 		//remove any remaining new-line characters
 			.replace( /[\n\r]/g, '' );
 	}
-
-	function saveEnd ( err ) {
-		if ( err ) {
-			throw err;
-		}
-
-		fs.stat( originalName, function ( err, stats ) {
-			if ( err ) {
-				throw err;
-			}
-			console.log( 'Original: ' + stats.size );
-		});
-
-		fs.stat( compressedName, function ( err, stats ) {
-			if ( err ) {
-				throw err;
-			}
-			console.log( 'Compressed: ' + stats.size );
-		});
-	}
 };
 
-console.log( 'Reading file...' );
-fs.readFile( 'game.js', function ( err, data ) {
+
+console.log( 'reading file...' );
+fs.readFile( originalName, function ( err, data ) {
 	if ( err ) {
 		throw err;
 	}
-	data = data.toString();
-
-	compile({
-		js_code : data,
-		compilation_level : 'SIMPLE_OPTIMIZATIONS',
-		output_info : 'compiled_code',
-		output_format : 'json'
+	compile( data.toString(), function ( compiledCollection ) {
+		console.log( '\nfinished all compilations\n' );
+		var smallest = chooseSmallest( compiledCollection );
+		saveCompiled( smallest );
 	});
 });
+
+function chooseSmallest ( compiledCollection ) {
+	return compiledCollection.sort(function ( a, b ) {
+		return a.length - b.length;
+	})[ 0 ];
+}
+
+function saveCompiled ( compiled ) {
+	console.log( 'saving compressed code...' );
+	fs.writeFile( compressedName, compiled, saveEnd );
+}
+
+function saveEnd ( err ) {
+	if ( err ) {
+		throw err;
+	}
+
+	fs.stat( originalName, function ( err, stats ) {
+		if ( err ) {
+			throw err;
+		}
+		console.log( 'original size: ' + stats.size );
+	});
+
+	fs.stat( compressedName, function ( err, stats ) {
+		if ( err ) {
+			throw err;
+		}
+		console.log( 'compressed size: ' + stats.size );
+	});
+}
